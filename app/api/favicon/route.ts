b539b347826a2a330098cf0215ad5adc0b407922
Lninn <thl384458870@outlook.com>
2024-04-08 21:08:04 +0800

    fix: rm  unValid Route export field.

--- app/api/favicon/route.ts
+++ app/api/favicon/route.ts
@@ -173,7 +173,7 @@ async function fetchImageFromSearchResult(taskInfo: WebScrapingTask) {
  * @param url 图片链接
  * @returns 返回一个Promise，解析为true表示是有效图片，否则为false。
  */
-export async function isImageValid(url: string): Promise<boolean> {
+async function isImageValid(url: string): Promise<boolean> {
   return new Promise<boolean>((resolve, reject) => {
     https.get(url, (res) => {
       const contentType = res.headers['content-type'];
